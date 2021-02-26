cf1e778234ece9bc0328b95bed8fa6bf585fa732
JoeyJaySWE <joeyjay@joeyjaydigital.com>
2021-02-26 12:47:42 +0100

    Added a timer function to allow smother transition

--- app/js/buttons.js
+++ app/js/buttons.js
@@ -2,5 +2,9 @@ const secreteBtn = document.querySelector(".secret button");
 
 secreteBtn.addEventListener("click", function(){
     const secret = document.querySelector(".secret");
-    secret.style.display = "none";
+    secret.classList.add("hidden");
+    setTimeout(function(){
+        console.log(secret);
+        secret.style.display = "none";
+    }, 700);
 })
