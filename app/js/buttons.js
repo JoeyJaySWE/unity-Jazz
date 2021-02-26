465f115f528ce3b835e76173d31d64504abc3e29
JoeyJaySWE <joeyjay@joeyjaydigital.com>
2021-02-26 11:57:52 +0100

    Added a feature for hiding code progress

---
+++ app/js/buttons.js
@@ -0,0 +1,6 @@
+const secreteBtn = document.querySelector(".secret button");
+
+secreteBtn.addEventListener("click", function(){
+    const secret = document.querySelector(".secret");
+    secret.style.display = "none";
+})
