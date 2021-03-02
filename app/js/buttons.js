a115a2f0a6c06b4f1a0589244c9cdab28cc0b384
JoeyJaySWE <joeyjay@joeyjaydigital.com>
2021-03-02 17:18:49 +0100

    Implmented greybox design for phone

--- app/js/buttons.js
+++ app/js/buttons.js
@@ -1,7 +1,14 @@
+const cookie = getCookie("swear");
+if(cookie === "true"){
+    console.log("Cookie set");
+    document.querySelector(".secret").style.display = "none";
+}
+
 const secreteBtn = document.querySelector(".secret button");
 
 secreteBtn.addEventListener("click", function(){
     const secret = document.querySelector(".secret");
+    document.cookie = "swear=true";
     secret.classList.add("hidden");
     setTimeout(function(){
         console.log(secret);
